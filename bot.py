50f58f24796c8e73b3d47c98d88aa4a4fe95ad64
Hiko <xikomiqaelyan@gmail.com>
2024-10-23 21:42:23 +0200

    API_ GPT

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 
+import openai
 import telebot
-from config import BOT_TOKEN
+from config import BOT_TOKEN, OPENAI_APY_KEY
 
 bot = telebot.TeleBot(BOT_TOKEN)
+openai.api_key = OPENAI_APY_KEY
 
 
 # Greetings
@@ -12,6 +14,34 @@ def start_command(message):
                      "Hello, I`m your basic assistant. Can i help you?")
 
 
+# Processing text message
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    response = generate_gpt_response(message.text)
+    bot.send_message(message.chat.id, response)
+
+
+# Function generate GPT response
+def generate_gpt_response(user_input):
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=150,
+            n=1,
+            temperature=0.7
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message,
+                 f"Hi, You said: {message.text}")
+
+
 # Started bot
 if __name__ == '__main__':
-    bot.infinity_polling()
+    bot.polling(non_stop=True)
